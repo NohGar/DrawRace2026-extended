2f9ee4e21efb13044a3d779c41c6a37781ddbe6f
NohGar <njwwn193@naver.com>
2026-05-04 13:40:22 +0900

    fix: WebMvcTest 컨텍스트에서 StorageProperties 빈 누락 문제 해결

--- src/main/java/backend/drawrace/global/config/WebMvcConfig.java
+++ src/main/java/backend/drawrace/global/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package backend.drawrace.global.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableConfigurationProperties(StorageProperties.class)
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final StorageProperties storageProperties;
